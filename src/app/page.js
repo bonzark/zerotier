27bda31001ef5bed3d0ce40934e76e6edac8328a
bonzark <hello@bonzark.com>
2023-08-29 15:26:54 +0530

    Button component FE

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
-import Image from 'next/image'
+import { Button } from "@/web/components";
 
 export default function Home() {
   return (
-    <h1>Home Page</h1>
-  )
+    <>
+      <h1>Home Page</h1>
+      <Button type="button" varient="ghost">
+        Some text
+      </Button>
+    </>
+  );
 }
