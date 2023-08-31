9f3401ebabe0fe69b34292ed9203654884bb846d
bonzark <hello@bonzark.com>
2023-08-31 10:41:27 +0530

    Footer component FE

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Navbar } from "@/web/components";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Footer } from "@/web/sections";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
